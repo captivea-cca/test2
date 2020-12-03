06440ade745b105c6de81abf851e768ec1c5461a
Chandler Calderon <72744609+captivea-cca@users.noreply.github.com>
2020-12-03 09:15:00 -0500

    Update project.py

--- cap_empty_stages/models/project.py
+++ cap_empty_stages/models/project.py
@@ -9,5 +9,8 @@ class Task(models.Model):
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
         search_domain = [('id', 'in', stages.ids)]
-        stage_ids = stages._search(search_domain, order=order, access_rights_uid=SUPERUSER_ID)
+        if 'default_project_id' in self.env.context:
+            search_domain = ['|', ('project_ids', '=', self.env.context['default_project_id'])] + search_domain
+
+        stage_ids = stages._search([], order=order, access_rights_uid=SUPERUSER_ID)
         return stages.browse(stage_ids)
